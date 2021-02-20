c94192b2bd65055d97c233c0af656a20f56cd83c
Commander_Cobra <gregawscsa@gmail.com>
2021-02-19 19:29:38 -0500

    testing new features

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -13,3 +17,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+
